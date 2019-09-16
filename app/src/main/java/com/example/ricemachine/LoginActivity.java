3b1fb42378ec190361ff2b0899d3472dc8be47f2
lyd0314 <372811231@qq.com>
2019-09-16 20:23:35 +0800

    1.首次提交

---
+++ app/src/main/java/com/example/ricemachine/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.ricemachine;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class LoginActivity extends BaseActivity implements View.OnClickListener{
+
+    private Button QRcode;//扫二维码按钮
+    private Button IDcard;//刷身份证按钮
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        QRcode.setOnClickListener(this);
+        IDcard.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.QRcode:
+                //在此添加逻辑
+                break;
+            case R.id.IDcard:
+                //在此添加逻辑
+                break;
+            default:
+                break;
+        }
+    }
+}
