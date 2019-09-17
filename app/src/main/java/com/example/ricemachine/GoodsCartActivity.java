ec917d21e809337ad0e100c9cc27f9de9831e2e6
lyd0314 <372811231@qq.com>
2019-09-17 13:36:33 +0800

    day9/17《2

---
+++ app/src/main/java/com/example/ricemachine/GoodsCartActivity.java
@@ -0,0 +1,14 @@
+package com.example.ricemachine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoodsCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_cart);
+    }
+}
