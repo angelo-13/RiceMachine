ed4455bdcb49e1aa140690b366190587de94ba2a
angelo-13 <2607487416@qq.com>
2019-09-19 08:11:55 +0800

    美化界面跟字体

--- app/src/main/java/com/example/ricemachine/MainActivity.java
+++ app/src/main/java/com/example/ricemachine/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.ricemachine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  {
 
     private Button login;
-
+    private TextView title;
+    private TextView company;
+    private TextView phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        title =findViewById(R.id.title);
+        company=findViewById(R.id.company);
+        phone=findViewById(R.id.phone);
         login = findViewById(R.id.login);
+        Typeface typeface= ResourcesCompat.getFont(this,R.font.whzzlsf);
+        login.setTypeface(typeface);
+        title.setTypeface(typeface);
+        phone.setTypeface(typeface);
+        login.setTypeface(typeface);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
